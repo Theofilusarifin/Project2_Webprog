c3d6031adf074b3edf7dece9ba4b59e186472d9b
Theofilusarifin <theofilusarifin@gmail.com>
2022-05-22 13:38:38 +0700

    Layout

---
+++ display.php
@@ -0,0 +1,28 @@
+<?php
+    $arrHape = array (
+        array(
+            "SKU"=>"12345", 
+            "Merk"=>"Samsung", 
+            "Model"=>"Galaxy S22", 
+            "Harga" => 18000000, 
+            "url_gambar"=>"....", 
+            "spec"=>array("4GB", "5G", "5150mAH", "256GB"
+        )), 
+        array(
+            "SKU"=>"54321", 
+            "Merk"=>"Xiaomi", 
+            "Model"=>"Mi 11 Pro", 
+            "Harga" => 9500000, 
+            "url_gambar"=>"....", 
+            "spec"=>array("8GB", "5G", "5050mAH", "256GB"
+        )), 
+        array(
+            "SKU"=>"AB012",
+            "Merk"=>"Redmi", 
+            "Model"=>"Redmi Note 12", 
+            "Harga"=> 6500000, 
+            "url_gambar"=>"....", 
+            "spec"=>array("6GB", "5G", "5350mAH", "256GB"
+        )),
+    )
+?>
--- index.php
+++ index.php
@@ -9,7 +9,15 @@
 </head>
 
 <body>
+    <p>Jumlah Kolom: </p>
 
+    <select name="jumlah_kolom">
+        <?php
+            for ($i=3; $i<7; $i++) { 
+                echo("<option value='$i'=> $i Kolom </option>");
+            }
+        ?>
+    </select>
 </body>
 
 </html>
