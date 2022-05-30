d49cd893781da96eee4d1b04b6117944b4b9342c
Theofilusarifin <theofilusarifin@gmail.com>
2022-05-30 15:22:35 +0700

    Update display.php

--- display.php
+++ display.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['arrHape'])) {
 
         body {
             background-color: #FFFFFF;
-            overflow: hidden;
+            overflow-x: hidden;
         }
 
         .rectangle {
